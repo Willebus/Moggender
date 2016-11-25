c89cdaddec5c255439aaaadc0d06356532460084
MrBulldozer <estiv97@gmail.com>
2016-11-25 14:57:07 +0100

    Logga in och Registrera Forms
    De gör inget just nu

--- index.php
+++ index.php
@@ -8,9 +8,10 @@
 <head>
     <meta charset="UTF-8">
 
+    <!--Override CSS-->
+    <link href="style.css" rel="stylesheet" type="text/css">
     <!--Bootstrap CSS-->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
     <!--Font Awesome CSS-->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
@@ -18,6 +19,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous" async></script>
     <!--jQuery till Bootstraps JavaScript-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" async></script>
+
     <title>Välkommen till Moggender</title>
 </head>
 <body>
--- start.php
+++ start.php
@@ -1,3 +1,24 @@
-<div class="well">
-    I'm feeling well!
+<div class="container">
+    <div class="row">
+        <div id="signin" class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <form class="form-signin" method="POST">
+                <h2 class="form-signin-heading">Logga in</h2>
+                <label for="inputUsername" class="sr-only">Användarnamn</label>
+                <input id="inputUsername" class="form-control" type="username" name="username"  placeholder="Användarnamn" required>
+                <label for="inputPassword" class="sr-only">Lösenord</label>
+                <input id="inputPassword" class="form-control" type="password" name="password"placeholder="Lösenord" required>
+                <a class="btn btn-block btn-primary" href="login.php">Logga in</a>
+            </form>
+        </div>
+        <div id="register" class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <form class="form-signin" method="POST">
+                <h2 class="form-signin-heading">Registrera</h2>
+                <label for="inputUsername" class="sr-only">Användarnamn</label>
+                <input id="inputUsername" class="form-control" type="username" name="username"  placeholder="Användarnamn" required>
+                <label for="inputPassword" class="sr-only">Lösenord</label>
+                <input id="inputPassword" class="form-control" type="password" name="password" placeholder="Lösenord" required>
+                <button class="btn btn-block btn-primary" type="submit">Registrera</button>
+            </form>
+        </div>
+    </div>
 </div>
