9064f4fda6a52d4f2a24c0985b875bbdb3aac146
Willebus <wonka9800@gmail.com>
2016-11-28 13:43:13 +0100

    Removed skit and fixed printing out month

--- calendar.php
+++ calendar.php
@@ -5,33 +5,71 @@
     <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <?php
-$date = getdate();
-$day = $date['mday'];
-$month = $date['mon'];
-$year = $date['year'];
-$firstOfMonth = date('01-'.$month.'-'.$year);
-$selectedDay = date($day.'-'.$month.'-'.$year);
-$dayFirstOfMonth = date('D', strtotime($firstOfMonth));
-$weekOfYear = date('W', strtotime($firstOfMonth));
-$daysInMonth = cal_days_in_month(0, $month, $year);
-switch($dayFirstOfMonth){
-    case "Sun": $blank = 0; break;
-    case "Mon": $blank = 1; break;
-    case "Tue": $blank = 2; break;
-    case "Wed": $blank = 3; break;
-    case "Thu": $blank = 4; break;
-    case "Fri": $blank = 5; break;
-    case "Sat": $blank = 6; break;
+$date = getdate(); //Kollar datumet just nu
+$day = $date['mday']; //Kollar dagen på $date
+$month = $date['mon']; // Kollar månaden på $date
+$year = $date['year']; // Kollar året på $date
+$firstOfMonth = date('01-'.$month.'-'.$year); // Om man väljer $month och $year sätter den ut 1:a dagen i månaden
+$selectedDay = date($day.'-'.$month.'-'.$year); // Denna variabeln ska användas för att få ut data på en specific dag
+$dayFirstOfMonth = date('D', strtotime($firstOfMonth)); // Denna variabeln kollar dagen som är den 1:a i månaden
+switch($dayFirstOfMonth){ // Detta är en switch som kollar både bilken dag det är som den 1:a är, men sätter även gråa rutor bakom om det finns tomma rutor.
+    case "Mon": $blank = 0; break; // Om det är söndag, sätter den inga gråa rutor bakom
+    case "Tue": $blank = 1; break; // Om det är måndag sätter den en grå ruta bakom
+    case "Wed": $blank = 2; break;
+    case "Thu": $blank = 3; break;
+    case "Fri": $blank = 4; break;
+    case "Sat": $blank = 5; break;
+    case "Sun": $blank = 6; break;
 }
+$weekOfYear = date('W', strtotime($firstOfMonth)); // Den sätter ut veckan på den första i månaden, inte klar än?
+$daysInMonth = cal_days_in_month(0, $month, $year); // Denna kollar hur många dagar det finns i månaden du valt.
 
-$dayCount = 1;
+$weekdayCount = 1;
+$dayNum = 1;
 
 echo $firstOfMonth."<br/>";
 echo $dayFirstOfMonth."<br/>";
 echo $daysInMonth."<br/>";
 echo $selectedDay."<br/>";
 ?>
+
 <body>
-    <table class="table"></table>
+    <table class="table">
+        <tr>
+            <th>Mån</th>
+            <th>Tis</th>
+            <th>Ons</th>
+            <th>Tors</th>
+            <th>Fre</th>
+            <th>Lör</th>
+            <th>Sön</th>
+        </tr>
+        <tr>
+          <?php
+          for($i = 0; $i <= $blank-1; $i++)
+          {
+              echo"<td>Nope</td>";
+              $weekdayCount++;
+          }
+              while($weekdayCount <= 7) // Den printar ut dagarna i veckan och sätter siffra på det.
+              {
+                  echo "<td>".$dayNum."</td>";
+                  $weekdayCount++;
+                  $dayNum++;
+                  if($weekdayCount == 8)
+                  {
+                      echo "</tr></tr>";
+                      $weekdayCount = 1;
+
+                  }
+                  if($dayNum == $daysInMonth+1)
+                  {
+                      break;
+                  }
+              }
+
+          ?>
+        </tr>
+    </table>
 </body>
 
